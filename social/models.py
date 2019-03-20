60298adf7fc782ca230b823ebb8771886e9a2082
StephenMlz <13599920526@163.com>
2019-03-20 23:20:36 +0800

    添加了swiped模型

--- social/models.py
+++ social/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Swiped(models.Model):
+    FLAGS = (
+        ('like','喜欢'),
+        ('superlike','超级喜欢'),
+        ('dislike','不喜欢')
+    )
+    uid = models.IntegerField(verbose_name='滑动者的uid')
+    sid = models.IntegerField(verbose_name='被滑动者的sid')
+    flag = models.CharField(max_length=16,choices=FLAGS,verbose_name='滑动类型')
+    time = models.DateTimeField(auto_now_add=True,verbose_name='滑动时间')
